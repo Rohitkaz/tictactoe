925953955efee025bad53a558968a7ae1d80f813
Rohit <www.rahulveeru12345@gmail.com>
2023-05-04 20:28:32 +0530

    commit2

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function start()
 {
  window.location.href="tictactoe.html";
 }
-window.onload=function(){
-    document.getElementById("mus").play();
-}
+
 var player1=0;
 var player2=0;
 function play()
